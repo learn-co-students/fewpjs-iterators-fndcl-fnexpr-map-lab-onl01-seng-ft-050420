1926dad5d608e148aeb8382a5a0316a3239726cb
codewithirene567 <irenejinscott@gmail.com>
2020-08-17 16:39:16 -0400

    Done.

--- index.js
+++ index.js
@@ -16,4 +16,10 @@ tutorial.split(" ").map(
  tutorial => tutorial[0].toUpperCase() + tutorial.slice(1)
 ).join(' '))
 
+//const titleCased = tutorials.map(function titleCased(tutorial){
+ // tutorial.toLowerCase().split(" ")
+ // return (tutorial[0].toUpperCase() + tutorial.slice(1))
+ // })
+
+  //=> ["What does the this keyword mean?", "What is the Contutorialuctor OO pattern?", "Implementing Blockchain Web API", "The Test Driven Development Workflow", "What is NaN and how Can we Check for it", "What is the difference between stopPropagation and preventDefault?", "Immutable State and Pure Functions", "What is the difference between == and ===?", "What is the difference between event capturing and bubbling?", "What is JSONP?"]
 //tutorials => tutorials[0].toUpperCase)
